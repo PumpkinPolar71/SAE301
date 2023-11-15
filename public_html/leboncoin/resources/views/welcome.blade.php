3da09cf8b147f1c18d80d4ade954c5f30a688d1f
Valentin <pumpkinpolar71@gmail.com>
2023-11-15 16:46:09 +0000

    a

---
+++ public_html/leboncoin/resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+
+@extends('layouts.app')
+
+@section('title', 'NageWaza')
+
+@section('sidebar')
+    @parent
+
+   
+
+
+@endsection
+
+
+
+
+@section('content')
+<h2>Bienvenue</h2>
+
+{{ $todays }}
+
+
+
+
+
+
+</form>
+
+
+@endsection
