d673d726ec19d7a91e0831625a885ed1c7a77df0
Valentin <pumpkinpolar71@gmail.com>
2023-11-17 12:28:06 +0000

    voila

--- public_html/leboncoin/resources/views/connect.blade.php
+++ public_html/leboncoin/resources/views/connect.blade.php
@@ -1,19 +1,4 @@
 @extends('layouts.app')
 
-<!--@section('title', 'LeBonCoin')-->
-<!-- 
-@section('sidebar')
-    @parent
 
-   
-
-
-@endsection -->
-
-
-
-
-@section('content')
-
-
-<h1>Se connecter</h1>
+<div="" >LeBonCoin</div>
