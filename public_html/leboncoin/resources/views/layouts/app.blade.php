40a77ee22d5f7616087bd78b9dd2d0da8a8c6a2a
Valentin <pumpkinpolar71@gmail.com>
2023-11-17 10:39:20 +0000

    save

--- public_html/leboncoin/resources/views/layouts/app.blade.php
+++ public_html/leboncoin/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="/recherche.js"></script>
+        
         <title>@yield('title')</title>
 
 
@@ -13,7 +13,7 @@
 
 
     <body>
-
+    <script src="recherche.js" defer></script>
     	<header>
     		<!-- <h1>@yield('title')</h1> -->
     	</header>
